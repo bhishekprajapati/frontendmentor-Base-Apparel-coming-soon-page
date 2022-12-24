7bd7a52b7d7a2cd6abf5a329dd4930456bc91953
Abhishek Prajapati <91626944+bhishekprajapati@users.noreply.github.com>
2022-12-24 14:06:35 +0530

    challenege complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+      },
+      colors: {
+        light: '#fef7f9',
+        dark: '#43393b',
+        beige: '#cc9d9f',
+        lightCoral: '#ec7484',
+        lightSalmon: '#dd9e8b',
+      },
+    },
   },
   plugins: [],
 }
